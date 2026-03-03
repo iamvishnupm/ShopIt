8c52262ee9c073ef093fe0cbe5cd49700ba7012d
Vishnu <i.am.vishnupm@gmail.com>
2026-03-03 20:54:45 +0530

    saved current user data, added .gitignore etc...

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -2,6 +2,8 @@
     import {goto} from "$app/navigation";
     import {onMount} from "svelte";
 
+    import {currentUser} from "$lib/stores/auth";
+
     onMount(
         async () => {
             const token = localStorage.getItem("shopit_token");
@@ -11,29 +13,40 @@
             }
 
             try {
-                const resp = await fetch(
-                    "http://127.0.0.1:8000/me",
-                    {
-                        headers : {
-                            "Authorization" : `Bearer ${token}`
-                        }
+                const resp = await fetch("http://127.0.0.1:8000/me", {
+                    headers: {
+                        Authorization: `Bearer ${token}`
                     }
-                );
+                });
 
-                if(!resp.ok){
+                if (!resp.ok) {
                     localStorage.removeItem("shopit_token");
-
-                    console.log("Error");
-                    console.log(resp);
-
-                    goto("/login");
+                    await goto("/login");
+                    return;
                 }
-            }
-            catch(err) {
-                console.log(err);
+
+                const user = await resp.json();
+
+                console.log("Authenticated user:", user);
+
+                // storing user details for future use.
+                currentUser.set(user);
+
+                console.log("User details saved");
+
+            } catch (err) {
+                console.error("Auth check failed:", err);
+                localStorage.removeItem("shopit_token");
+                await goto("/login");
             }
         }
     );
+
+
 </script>
 
 <h1>ShopIt</h1>
+{#if $currentUser}
+  <p>username : {$currentUser.username}</p>
+  <p>email : {$currentUser.email}</p>
+{/if}
