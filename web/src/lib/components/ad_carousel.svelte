a555caa5b48bf98d56f9baea6bf019d0558b4955
Vishnu <i.am.vishnupm@gmail.com>
2026-03-04 19:38:03 +0530

    setup ad-carousel

--- web/src/lib/components/ad_carousel.svelte
+++ web/src/lib/components/ad_carousel.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
+	let index = $state(0);
 	let { variant = 'large' } = $props();
 
 	let ads = [
 		'https://picsum.photos/1200/300?random=1',
 		'https://picsum.photos/1200/300?random=2',
-		'https://picsum.photos/1200/300?random=3'
+		'https://picsum.photos/1200/300?random=3',
+		'https://picsum.photos/1200/300?random=4',
+		'https://picsum.photos/1200/300?random=5'
 	];
-
-	let index = $state(0);
 
 	function goTo(i: number) {
 		index = i;
@@ -17,10 +18,11 @@
 
 	onMount(() => {
 		const interval = setInterval(
+			//
 			() => {
 				index = (index + 1) % ads.length;
 			},
-			variant === 'large' ? 4000 : 7000
+			4000
 		);
 
 		return () => clearInterval(interval);
@@ -37,8 +39,11 @@
 		>
 			{#each ads as ad}
 				<img
+					//
+					alt=""
 					src={ad}
-					class={`w-full flex-shrink-0 object-cover ${variant === 'large' ? 'h-64' : 'h-40'}`}
+					class={`w-full shrink-0 rounded-lg object-cover
+							${variant === 'large' ? 'h-80' : 'h-40'}`}
 				/>
 			{/each}
 		</div>
@@ -47,10 +52,12 @@
 	<!-- dots -->
 	<div class="mt-3 flex justify-center gap-2">
 		{#each ads as _, i}
-			<button
-				class={`h-2 w-2 rounded-full transition ${i === index ? 'bg-gray-800' : 'bg-gray-400'}`}
+			<input
+				type="button"
 				onclick={() => goTo(i)}
-			></button>
+				class={`h-2 w-2 rounded-full transition 
+						${i === index ? 'bg-gray-800' : 'bg-gray-400'}`}
+			/>
 		{/each}
 	</div>
 </div>
